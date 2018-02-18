fff8583e395165eca363124bbc554bb91a447e3b
Wojtek Tartanus <wojtek.tartanus@airts.co.uk>
2018-02-18 18:29:24 +0000

    moved seeding to client, also fixed header

--- frontend/client/src/app.jsx
+++
@@ -1,7 +0,0 @@
-var React = require('react');
-var ReactDom = require('react-dom');
-var ShopBox = require('./components/ShopBox.jsx');
-
-window.onload = function() {
-  ReactDom.render(<ShopBox url="http://localhost:4567/"  />,document.getElementById('app'));
-};
