f958a469d0c9557756f46717efa8070994bcd53f
Wojtek Tartanus <wojtek.tartanus@airts.co.uk>
2018-04-02 20:42:12 +0100

    Hookin up with model.

--- frontend/clientAngular/src/app/components/checkout.component.js
+++ frontend/clientAngular/src/app/components/checkout.component.js
@@ -14,6 +14,16 @@ var basket_service_js_1 = require("../services/basket.service.js");
 var CheckoutComponent = (function () {
     function CheckoutComponent(basket) {
         this.basket = basket;
+        this.delivery = "1";
+        this.deliveryTypes = {
+            "1": { name: "Royal Mail Second Class", cost: 2.71 },
+            "2": { name: "Royal Mail First Class", cost: 2.71 },
+            "3": { name: "Royal Mail Tracked48", cost: 3.28 },
+            "4": { name: "Royal Mail Tracked24", cost: 4.68 },
+            "5": { name: "DPD Express Next Day", cost: 5.05 },
+            "6": { name: "DPD Weekend Shipping", cost: 7.10 },
+            "7": { name: "Royal Mail Overnight", cost: 6.40 }
+        };
     }
     CheckoutComponent.prototype.ngOnInit = function () {
         console.log("@@@", this.basket);
--- frontend/clientAngular/src/app/components/checkout.component.ts
+++ frontend/clientAngular/src/app/components/checkout.component.ts
@@ -12,6 +12,24 @@ import { CategoriesService } from '../services/categories.service.js';
   templateUrl: 'src/app/views/checkout.component.html'
 })
 export class CheckoutComponent implements OnInit {
+  public email: string;
+  public firstName: string;
+  public lastName: string;
+  public addressOne: string;
+  public addressTwo: string;
+  public city: string;
+  public postcode: string; 
+  public delivery: string = "1";
+  private deliveryTypes: any = {
+    "1": {name: "Royal Mail Second Class", cost: 2.71},
+    "2": {name: "Royal Mail First Class", cost: 2.71},
+    "3": {name: "Royal Mail Tracked48", cost: 3.28},
+    "4": {name: "Royal Mail Tracked24", cost: 4.68},
+    "5": {name: "DPD Express Next Day", cost: 5.05},
+    "6": {name: "DPD Weekend Shipping", cost: 7.10},
+    "7": {name: "Royal Mail Overnight", cost: 6.40}
+  }
+
   constructor (public basket: BasketService) {}
 
   ngOnInit() {
